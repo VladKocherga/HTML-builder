f221c6a85d59f70445a5e5892675fd72e58f9a48
VladKocherga <vlad.kocherga228@gmail.com>
2021-10-31 14:39:11 +0300

    feat: end two task

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const chalk = require("chalk");
+const readLine = require("readline");
+const process = require("process");
+
+let streamTwo = new fs.createWriteStream("02-write-file/message.txt", {
+  encoding: "utf-8",
+});
+let inter = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: chalk.blue("Hello, please enter some text: \n"),
+});
+inter.prompt();
+
+inter.on("line", (input) => {
+  input.trim() == "exit" ? inter.close() : streamTwo.write(input + "\n");
+});
+inter.on("SIGINT", () => {
+  inter.close();
+});
+inter.on("close", () => {
+  console.log(chalk.blue("the end"));
+  streamTwo.end();
+});
